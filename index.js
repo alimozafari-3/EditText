d73cab77f543c34b98bf958102c671a0b74ddfb1
ali <ali.mozafari733@yahoo.com>
2023-02-10 17:15:29 +0330

    js file

---
+++ index.js
@@ -0,0 +1,33 @@
+let textarea = document.querySelector(".text");
+let sez = 18;
+function color(val) {
+  textarea.style.color = `${val}`;
+}
+
+function inc() {
+  sez++;
+  textarea.style.fontSize = `${sez}px`;
+}
+
+function dec() {
+  sez--;
+  textarea.style.fontSize = `${sez}px`;
+}
+
+function bold() {
+  textarea.style.fontWeight = "900";
+}
+
+function italic() {
+  textarea.style.fontStyle = "italic";
+}
+
+function left() {
+  textarea.style.textAlign = "left";
+}
+function center() {
+  textarea.style.textAlign = "center";
+}
+function right() {
+  textarea.style.textAlign = "right";
+}
